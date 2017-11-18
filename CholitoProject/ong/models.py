6cf253a5c7f49337c3a923d995afa1646f91b7e6
Lxopato <lpbustoscarrasco@gmail.com>
2017-11-18 20:58:25 -0300

    Add ONGUser fields, put permissions ins temp_instructions

--- CholitoProject/ong/models.py
+++ CholitoProject/ong/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import User
-
-# Create your models here.
+from django.contrib.auth.models import User, Permission
+from django.shortcuts import redirect
 
 
 class ONG(models.Model):
@@ -15,3 +12,18 @@ class ONG(models.Model):
 
 class ONGUser(User):
     ong = models.ForeignKey('ONG')
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.ong.name
+
+    def save(self, *args, **kwargs):
+        super(ONGUser, self).save(*args, **kwargs)
+        if not self.user.has_perm('ong_user_access'):
+            permission = Permission.objects.get(
+                codename='ong_user_access')
+            self.user.user_permissions.add(permission)
+        self.user.save()
+
+    def get_index(self, request, context):
+        return redirect('/ong/')
