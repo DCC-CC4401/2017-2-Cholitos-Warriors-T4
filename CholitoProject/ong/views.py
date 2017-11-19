683b69f3c38527f5e6463d4abaca051a7c815015
David <david.ra.157@gmail.com>
2017-11-19 19:59:10 -0300

    Building ONG template

--- CholitoProject/ong/views.py
+++ CholitoProject/ong/views.py
@@ -5,16 +5,19 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from CholitoProject.userManager import get_user_index
 from ong.models import ONG
+from animals.models import *
 from naturalUser.models import NaturalUser, FavoriteONGs
 # Create your views here.
 
 class ONGforNaturalUser(View):
     template = 'ong-adopcion-ya.html'
     context = {}
+
     def get(self, request, pk, **kwargs):
         user = get_user_index(request.user)
         self.context['user'] = user
         ong = get_object_or_404(ONG, pk=pk)
         self.context['ong'] = ong
+        self.context['animals'] = Animal.objects.filter(ong = ong.id)
 
         return render(request, self.template, context=self.context)
